143e544beb19f3143c6ab6b3531b0c47c4d92c17
Christian Stade-Schuldt <tafkasorg@yahoo.de>
2017-02-27 09:20:59 +0100

    Fix missing x-axis tick in weekly forecast component plot (#28)
    * prepend None value to list of days in weekly forecast plot
    
    * set the x ticks with list of days in weekly forecast component plot

--- python/fbprophet/forecaster.py
+++ python/fbprophet/forecaster.py
@@ -721,6 +721,7 @@ class Prophet(object):
                 ax.fill_between(range(len(days)), y_weekly_l, y_weekly_u,
                                 color=forecast_color, alpha=0.2)
             ax.grid(True, which='major', c='gray', ls='-', lw=1, alpha=0.2)
+            ax.set_xticks(range(len(days)))
             ax.set_xticklabels(days)
             ax.set_xlabel('Day of week')
             ax.set_ylabel('weekly')
