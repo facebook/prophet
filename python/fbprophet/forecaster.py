c7276f5c9f50adbc27583c52bc5e592efc1ff3db
Gautam <gautamrbharadwaj@gmail.com>
2017-03-01 11:49:45 +0530

    Update forecaster.py
    Adding a new forecast algorithm

--- python/fbprophet/forecaster.py
+++ python/fbprophet/forecaster.py
@@ -731,5 +731,98 @@ class Prophet(object):
 
         fig.tight_layout()
         return fig
+    
 
 # fb-block 9
+
+import math
+import numpy  as np
+import pandas as pd
+
+from sklearn import linear_model
+from scipy.optimize import fmin_l_bfgs_b
+
+def data():
+    ts = pd.read_sql("select no_of_request from timeseries", settings.engine)
+    ts = np.array(ts)
+
+    return ts
+
+def gaussian(ts, p, sp, ahead, mtype, alpha=None, beta=None, gamma=None):
+
+    a, b, s = _initialize(mtype, ts, p, sp)
+    if alpha == None or beta == None or gamma == None:
+        ituning = [0.1, 0.1, 0.1]
+        ibounds = [(0, 1), (0, 1), (0, 1)]
+        optimized = fmin_l_bfgs_b(_MSD, ituning, args=(mtype, ts, p, a, b, s[:]), bounds=ibounds, approx_grad=True)
+        alpha, beta, gamma = optimized[0]
+
+    MSD, params, smoothed = _filter(mtype, ts, p, a, b, s[:], alpha, beta, gamma)
+    forecasted = _forecast(mtype, p, ahead, params)
+
+    return forecasted
+
+
+def _initialize(mtype, ts, p,sp):
+
+    initSeries = ts
+
+    mtype == 'gaussian'
+
+    rawSeason = initSeries / pd.rolling_mean(initSeries, window=p, min_periods=p, center=True)
+    initSeason = [np.nanmean(rawSeason[i::p]) for i in range(p)]
+    initSeason = pd.Series(initSeason) / math.pow(np.prod(np.array(initSeason)), 1 / p)
+    deSeasoned = [initSeries[v] / initSeason[v % p] for v in range(len(initSeries))]
+
+    lm = linear_model.LinearRegression()
+    lm.fit(pd.DataFrame({'time': [t + 1 for t in range(len(initSeries))]}), pd.Series(deSeasoned))
+    return float(lm.intercept_), float(lm.coef_), list(initSeason)
+
+
+def _MSD(tuning, *args):
+
+    predicted = []
+    mtype = args[0]
+    ts, p = args[1:3]
+    Lt1, Tt1 = args[3:5]
+    St1 = args[5][:]
+    alpha, beta, gamma = tuning[:]
+
+    for t in range(len(ts)):
+        mtype == 'gaussian'
+        Lt = alpha * (ts[t] / St1[t % p]) + (1 - alpha) * (Lt1 + Tt1)
+        Tt = beta * (Lt - Lt1) + (1 - beta) * (Tt1)
+        St = gamma * (ts[t] / Lt) + (1 - gamma) * (St1[t % p])
+        predicted.append(((Lt1 + Tt1) * St1[t % p]).item(0))
+
+        Lt1, Tt1, St1[t % p] = Lt, Tt, St
+
+    return sum([(ts[t] - predicted[t]) ** 2 for t in range(len(predicted))]) / len(predicted)
+
+
+def _filter(mtype, ts, p, a, b, s, alpha, beta, gamma):
+    smoothed = []
+    Lt1, Tt1, St1 = a, b, s[:]
+
+    for t in range(len(ts)):
+        mtype == 'gaussian'
+
+        Lt = alpha * (ts[t] / St1[t % p]) + (1 - alpha) * (Lt1 + Tt1)
+        Tt = beta * (Lt - Lt1) + (1 - beta) * (Tt1)
+        St = gamma * (ts[t] / Lt) + (1 - gamma) * (St1[t % p])
+        smoothed.append(((Lt1 + Tt1) * St1[t % p]).item(0))
+
+        Lt1, Tt1, St1[t % p] = Lt, Tt, St
+
+    MSD = sum([(ts[t] - smoothed[t]) ** 2 for t in range(len(smoothed))]) / len(smoothed)
+    return MSD, (Lt1, Tt1, St1), smoothed
+
+def _forecast(mtype, p, ahead, params):
+    Lt, Tt, St = params
+    mtype == 'gaussian'
+    return [(Lt.item(0) + (t + 1) * Tt.item(0)) * St[t % p].item(0) for t in range(ahead)]
+
+# Main function 
+def forecas(no_of_request, no_of_period):
+    results = gaussian(no_of_request, 12, len(no_of_request), no_of_period, mtype='gaussian')
+    return results
