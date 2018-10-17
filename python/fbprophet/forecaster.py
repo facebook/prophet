bea89505d67e67dd1e5a6c9e27cfcf72c2d4729c
Tomasini Marcello <whitehatty@gmail.com>
2018-10-17 13:27:26 -0700

    fix issue #700: duplicate kwarg algorithm

--- python/fbprophet/forecaster.py
+++ python/fbprophet/forecaster.py
@@ -1047,6 +1047,7 @@ class Prophet(object):
                 params = model.optimizing(
                     dat, init=stan_init, iter=1e4, **kwargs)
             except RuntimeError:
+		kwargs.pop('algorithm')
                 params = model.optimizing(
                     dat, init=stan_init, iter=1e4, algorithm='Newton',
                     **kwargs
