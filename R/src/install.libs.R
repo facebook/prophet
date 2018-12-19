7af93c9f19357d7a49a2cc9803aff53e15502be4
Jim Hester <james.f.hester@gmail.com>
2018-12-19 13:45:04 -0500

    Suppress pkgbuild messages
    Fixes https://github.com/r-lib/pkgbuild/issues/54

--- R/src/install.libs.R
+++ R/src/install.libs.R
@@ -10,11 +10,16 @@ packageStartupMessage(paste('Compiling using binary:', R.home('bin')))
 
 model.src <- file.path(R_PACKAGE_SOURCE, 'inst', 'stan', 'prophet.stan')
 model.binary <- file.path(dest, 'prophet_stan_model.RData')
-model.stanc <- rstan::stanc(model.src)
-model.stanm <- rstan::stan_model(
-  stanc_ret = model.stanc,
-  model_name = 'prophet_model'
-)
+
+
+suppressMessages({
+  model.stanc <- rstan::stanc(model.src)
+  model.stanm <- rstan::stan_model(
+    stanc_ret = model.stanc,
+    model_name = 'prophet_model'
+  )
+})
+
 save('model.stanm', file = model.binary)
 
 packageStartupMessage('------ Model successfully compiled!')
