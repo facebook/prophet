39e356ddbeb78c60585dfe3ac8a7d5bee274d41b
Sean J. Taylor <sjt@fb.com>
2019-05-13 23:00:12 -0700

    Readying for 0.5 release

--- python/setup.py
+++ python/setup.py
@@ -97,11 +97,11 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='fbprophet',
-    version='0.4.post2',
+    version='0.5',
     description='Automatic Forecasting Procedure',
     url='https://facebook.github.io/prophet/',
-    author='Sean J. Taylor <sjt@fb.com>, Ben Letham <bletham@fb.com>',
-    author_email='sjt@fb.com',
+    author='Sean J. Taylor <sjtz@pm.me>, Ben Letham <bletham@fb.com>',
+    author_email='sjtz@pm.me',
     license='BSD',
     packages=find_packages(),
     setup_requires=[
