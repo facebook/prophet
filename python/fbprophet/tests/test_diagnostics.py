4225bb5fc1d848844a2b332984dcb717730d7377
Louis Potok <louispotok@gmail.com>
2019-05-28 17:53:22 -0700

    Add mdape to all diagnostics, and fix rolling median behavior.

--- python/fbprophet/tests/test_diagnostics.py
+++ python/fbprophet/tests/test_diagnostics.py
@@ -114,7 +114,7 @@ class TestDiagnostics(TestCase):
         df_none = diagnostics.performance_metrics(df_cv, rolling_window=-1)
         self.assertEqual(
             set(df_none.columns),
-            {'horizon', 'coverage', 'mae', 'mape', 'mse', 'rmse'},
+            {'horizon', 'coverage', 'mae', 'mape', 'mdape', 'mse', 'rmse'},
         )
         self.assertEqual(df_none.shape[0], 16)
         # Aggregation level 0
@@ -130,6 +130,7 @@ class TestDiagnostics(TestCase):
         self.assertEqual(df_all.shape[0], 1)
         for metric in ['mse', 'mape', 'mae', 'coverage']:
             self.assertAlmostEqual(df_all[metric].values[0], df_none[metric].mean())
+        self.assertAlmostEqual(df_all['mdape'].values[0], df_none['mdape'].median())
         # Custom list of metrics
         df_horizon = diagnostics.performance_metrics(
             df_cv, metrics=['coverage', 'mse'],
