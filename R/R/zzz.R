46e56119835f851714d22b285d2e4081853b9fb1
Ben Letham <bletham@gmail.com>
2020-02-19 21:30:08 -0800

    R defer model compiling to first fit, to remove compiling from onLoad per CRAN

--- R/R/zzz.R
+++ R/R/zzz.R
@@ -4,10 +4,28 @@
 # LICENSE file in the root directory of this source tree.
 
 .onLoad <- function(libname, pkgname) {
-  .prophet.stan.model <- get_prophet_stan_model()
-  assign(
-    ".prophet.stan.model",
-    .prophet.stan.model,
-    envir=parent.env(environment())
+  # Create environment for storing stan model
+  assign("prophet_model_env", new.env(), parent.env(environment()))
+  tryCatch({
+    binary <- system.file(
+      'libs',
+      'prophet_stan_model.RData',
+      package = 'prophet',
+      mustWork = TRUE
+    )
+    load(binary)
+    obj.name <- 'model.stanm'
+    stanm <- eval(parse(text = obj.name))
+
+    ## Should cause an error if the model doesn't work.
+    stanm@mk_cppmodule(stanm)
+
+    assign(
+      ".prophet.stan.model",
+      stanm,
+      envir=prophet_model_env
+    )
+  },
+  error = function(cond) {}
   )
 }
