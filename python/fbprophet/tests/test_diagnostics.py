ea26eac9500b923e01a9568289763c55632c95d8
Fusi Marco <Marco.Fusi@valuelab.it>
2020-03-25 01:43:37 +0100

    modified to test lengths instread of cutoff values

--- python/fbprophet/tests/test_diagnostics.py
+++ python/fbprophet/tests/test_diagnostics.py
@@ -141,9 +141,8 @@ class TestDiagnostics(TestCase):
             horizon='32 days',
             period='10 days',
             cutoffs=[pd.Timestamp('2012-07-31'), pd.Timestamp('2012-08-31')])
-        self.assertItemsEqual(
-            (df_cv1['cutoff'].unique(), [pd.Timestamp('2012-07-31'), pd.Timestamp('2012-08-31')]))  
-        
+        self.assertEqual(len(df_cv1['cutoff'].unique()), 2)
+      
     def test_cross_validation_uncertainty_disabled(self):
         df = self.__df.copy()
         for uncertainty in [0, False]:
