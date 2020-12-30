1409fa13a7649bc168e620a30e8eadc085ad25f0
Sam Snarr <sssnarr1@gmail.com>
2020-12-29 23:33:42 -0500

    used regex to format the `name`
    Removes all singlequotes (') and doublequotes (") in the `name` variable
    and replaces all whitespace with an underscore. Now, `ggplot2::aes_string()` can handle a column
    name like `New Year's Day`.

--- R/R/plot.R
+++ R/R/plot.R
@@ -177,8 +177,11 @@ prophet_plot_components <- function(
 plot_forecast_component <- function(
   m, fcst, name, uncertainty = TRUE, plot_cap = FALSE
 ) {
+  formatted.name <- gsub("'|\"", "", name)
+  formatted.name <- gsub("\\s+", "_", formatted.name)
+
   gg.comp <- ggplot2::ggplot(
-      fcst, ggplot2::aes_string(x = 'ds', y = name, group = 1)) +
+      fcst, ggplot2::aes_string(x = 'ds', y = formatted.name, group = 1)) +
     ggplot2::geom_line(color = "#0072B2", na.rm = TRUE)
   if (exists('cap', where = fcst) && plot_cap) {
     gg.comp <- gg.comp + ggplot2::geom_line(
@@ -192,7 +195,7 @@ plot_forecast_component <- function(
     gg.comp <- gg.comp +
       ggplot2::geom_ribbon(
         ggplot2::aes_string(
-          ymin = paste0(name, '_lower'), ymax = paste0(name, '_upper')
+          ymin = paste0(formatted.name, '_lower'), ymax = paste0(formatted.name, '_upper')
         ),
         alpha = 0.2,
         fill = "#0072B2",
