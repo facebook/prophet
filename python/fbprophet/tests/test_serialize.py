49a89d2a4e3d445c3d3c4264815cb432bf7abb72
Ben Letham <bletham@gmail.com>
2021-03-04 20:12:34 -0800

    Bump version in unit test

--- python/fbprophet/tests/test_serialize.py
+++ python/fbprophet/tests/test_serialize.py
@@ -41,7 +41,7 @@ class TestSerialize(TestCase):
         # Make sure json doesn't get too large in the future
         self.assertTrue(len(model_str) < 200000)
         z = json.loads(model_str)
-        self.assertEqual(z['__fbprophet_version'], '0.7.1')
+        self.assertEqual(z['__fbprophet_version'], '1.0')
 
         m2 = model_from_json(model_str)
 
