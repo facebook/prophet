612934695bf42835261804a12bcc1a8a9ea31176
Ben Letham <bletham@gmail.com>
2021-03-04 17:16:23 -0800

    Version bump

--- python/setup.py
+++ python/setup.py
@@ -122,7 +122,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='fbprophet',
-    version='0.7.1',
+    version='1.0',
     description='Automatic Forecasting Procedure',
     url='https://facebook.github.io/prophet/',
     author='Sean J. Taylor <sjtz@pm.me>, Ben Letham <bletham@fb.com>',
