d0930664378d8d947cd5fff6554dfcd1f76b5cb0
Vladimir Shargin <me@vshargin.ru>
2021-03-27 23:30:13 +0300

    Add include_legend flag to m.plot()

--- python/prophet/plot.py
+++ python/prophet/plot.py
@@ -41,7 +41,7 @@ except ImportError:
 
 def plot(
     m, fcst, ax=None, uncertainty=True, plot_cap=True, xlabel='ds', ylabel='y',
-    figsize=(10, 6)
+    figsize=(10, 6), include_legend=False
 ):
     """Plot the Prophet forecast.
 
@@ -57,6 +57,7 @@ def plot(
     xlabel: Optional label name on X-axis
     ylabel: Optional label name on Y-axis
     figsize: Optional tuple width, height in inches.
+    include_legend: Optional boolean to add legend to the plot.
 
     Returns
     -------
@@ -68,15 +69,16 @@ def plot(
     else:
         fig = ax.get_figure()
     fcst_t = fcst['ds'].dt.to_pydatetime()
-    ax.plot(m.history['ds'].dt.to_pydatetime(), m.history['y'], 'k.')
-    ax.plot(fcst_t, fcst['yhat'], ls='-', c='#0072B2')
+    ax.plot(m.history['ds'].dt.to_pydatetime(), m.history['y'], 'k.',
+            label='Observed data points')
+    ax.plot(fcst_t, fcst['yhat'], ls='-', c='#0072B2', label='Forecast')
     if 'cap' in fcst and plot_cap:
-        ax.plot(fcst_t, fcst['cap'], ls='--', c='k')
+        ax.plot(fcst_t, fcst['cap'], ls='--', c='k', label='Maximum capacity')
     if m.logistic_floor and 'floor' in fcst and plot_cap:
-        ax.plot(fcst_t, fcst['floor'], ls='--', c='k')
+        ax.plot(fcst_t, fcst['floor'], ls='--', c='k', label='Minimum capacity')
     if uncertainty and m.uncertainty_samples:
         ax.fill_between(fcst_t, fcst['yhat_lower'], fcst['yhat_upper'],
-                        color='#0072B2', alpha=0.2)
+                        color='#0072B2', alpha=0.2, label='Uncertainty interval')
     # Specify formatting to workaround matplotlib issue #12925
     locator = AutoDateLocator(interval_multiples=False)
     formatter = AutoDateFormatter(locator)
@@ -85,6 +87,8 @@ def plot(
     ax.grid(True, which='major', c='gray', ls='-', lw=1, alpha=0.2)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
+    if include_legend:
+        ax.legend()
     fig.tight_layout()
     return fig
 
