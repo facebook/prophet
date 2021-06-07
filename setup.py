afd8660adc1a2266c5bee505bcaa6ea5cc2120cc
Kaixu Yang <kaixuyang@gmail.com>
2021-06-07 15:35:15 -0400

    bumps version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='holidays-ext',
-    version='0.0.1',
+    version='0.0.2',
     description='Extended holidays package',
     url='https://github.com/kaixuyang/holidays-extension',
     author='Kaixu Yang',
