43213e10a06551893c4a7ed9372bce045024dcd5
Kaixu Yang <kayang@kayang-mn2.linkedin.biz>
2021-06-10 13:50:47 -0400

    added init

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='holidays-ext',
-    version='0.0.3',
+    version='0.0.4',
     description='Extended holidays package',
     url='https://github.com/kaixuyang/holidays-extension',
     author='Kaixu Yang',
