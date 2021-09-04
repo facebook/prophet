ee1abe738c32468286da8a066dda46cd961cf667
Cuong Duong <cuong@canva.com>
2021-09-05 02:32:00 +1000

    cache cmdstan files, make linux aware of extension modules, prune cmdstan logic

--- python/setup.py
+++ python/setup.py
@@ -3,11 +3,15 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import contextlib
+import subprocess
 import os
 import pickle
 import platform
 import sys
-from shutil import copy, rmtree
+from shutil import copy, copytree, rmtree
+from pathlib import Path
+from distutils.extension import Extension
 from pkg_resources import (
     normalize_path,
     working_set,
@@ -15,56 +19,157 @@ from pkg_resources import (
     require,
 )
 from setuptools import setup, find_packages
+from setuptools.command.build_ext import build_ext
 from setuptools.command.build_py import build_py
 from setuptools.command.develop import develop
 from setuptools.command.test import test as test_command
 from setuptools.dist import Distribution
-from typing import List
+from typing import List, Iterator
 
-PLATFORM = 'unix'
-if platform.platform().startswith('Win'):
-    PLATFORM = 'win'
+PLATFORM = "unix"
+if platform.platform().startswith("Win"):
+    PLATFORM = "win"
 
-MODEL_DIR = os.path.join('stan', PLATFORM)
-MODEL_TARGET_DIR = os.path.join('prophet', 'stan_model')
+MODEL_DIR = os.path.join("stan", PLATFORM)
+MODEL_TARGET_DIR = os.path.join("prophet", "stan_model")
+
+# cmdstan utils
 CMDSTAN_VERSION = "2.26.1"
+EXTENSION = ".exe" if platform.system() == "Windows" else ""
+MAKE = os.getenv("MAKE", "make" if platform.system() != "Windows" else "mingw32-make")
+BINARIES_DIR = "bin"
+BINARIES = ["diagnose", "print", "stanc", "stansummary"]
+TBB_PARENT = "stan/lib/stan_math/lib"
+TBB_DIRS = ["tbb", "tbb_2019_U8"]
+
+
+class CmdStanInstallError(RuntimeError):
+    pass
+
+
+@contextlib.contextmanager
+def pushd(new_dir: str) -> Iterator[None]:
+    """Acts like pushd/popd."""
+    previous_dir = os.getcwd()
+    os.chdir(new_dir)
+    yield
+    os.chdir(previous_dir)
+
+
+def build_cmdstan(cmdstan_dir: str, verbose: bool = False) -> None:
+    with pushd(cmdstan_dir):
+        cmd = [MAKE, "build"]
+        proc = subprocess.Popen(
+            cmd,
+            cwd=None,
+            stdin=subprocess.DEVNULL,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            env=os.environ,
+        )
+        while proc.poll() is None:
+            if proc.stdout:
+
+                output = proc.stdout.readline().decode("utf-8").strip()
+                if verbose and output:
+                    print(output, flush=True)
+        _, stderr = proc.communicate()
+        if proc.returncode:
+            msgs = ['Command "make build" failed']
+            if stderr:
+                msgs.append(stderr.decode("utf-8").strip())
+            raise CmdStanInstallError("\n".join(msgs))
+        if not os.path.exists(os.path.join("bin", "stansummary" + EXTENSION)):
+            raise CmdStanInstallError(
+                f"bin/stansummary{EXTENSION} not found"
+                ", please rebuild or report a bug!"
+            )
+        if not os.path.exists(os.path.join("bin", "diagnose" + EXTENSION)):
+            raise CmdStanInstallError(
+                f"bin/stansummary{EXTENSION} not found"
+                ", please rebuild or report a bug!"
+            )
+
+
+def prune_cmdstan(cmdstan_dir: str) -> None:
+    with pushd(cmdstan_dir):
+        # Create temp dir
+        temp_dir = Path(".").resolve().parent / "temp_dir"
+        if temp_dir.is_dir():
+            rmtree(temp_dir)
+        else:
+            temp_dir.mkdir()
+        # Copy minimal files to temp dir
+        copytree(Path(".") / BINARIES_DIR, temp_dir / BINARIES_DIR)
+        for f in (temp_dir / BINARIES_DIR).iterdir():
+            if f.is_dir():
+                rmtree(f)
+            elif f.is_file() and f.stem not in BINARIES:
+                os.remove(f)
+        for tbb_dir in TBB_DIRS:
+            copytree(Path(".") / TBB_PARENT / tbb_dir, temp_dir / tbb_dir)
+        # Remove everything inside cmdstan_dir
+        for f in Path(".").iterdir():
+            if f.is_dir():
+                rmtree(f)
+            elif f.is_file():
+                os.remove(f)
+        # Copy the necessary files back into cmdstan_dir
+        copytree(temp_dir / BINARIES_DIR, Path(".") / BINARIES_DIR)
+        for tbb_dir in TBB_DIRS:
+            copytree(temp_dir / tbb_dir, Path(".") / TBB_PARENT / tbb_dir)
+        rmtree(temp_dir)
+
 
 def get_backends_from_env() -> List[str]:
     return os.environ.get("STAN_BACKEND", "PYSTAN").split(",")
 
-def prune_cmdstan_files(cmdstan_dir):
-    """
-    Remove unnecessary folders from the unbundled cmdstan installation.
-    We only need to keep the files that will be used to execute the model binary at runtime.
-    """
-    remove_dirs = ["examples", "stan"]
-    for dirname in remove_dirs:
-        rmtree(os.path.join(cmdstan_dir, dirname))
-    raw_binaries = ["linux-stanc", "mac-stanc", "windows-stanc"]  # These are converted into "stanc" in the Make process
-    for fname in raw_binaries:
-        os.remove(os.path.join(cmdstan_dir, f"bin/{fname}"))
+
+def get_cmdstan_cache() -> str:
+    return Path.home().resolve() / ".cmdstan" / f"cmdstan-{CMDSTAN_VERSION}"
+
 
 def build_cmdstan_model(target_dir):
     import cmdstanpy
-    cmdstanpy.install_cmdstan(version=CMDSTAN_VERSION, dir=target_dir, overwrite=True)
+
+    cmdstan_cache = get_cmdstan_cache()
     cmdstan_dir = os.path.join(target_dir, f"cmdstan-{CMDSTAN_VERSION}")
+
+    if os.path.isdir(cmdstan_cache):
+        print(f"Found existing cmdstan library at {cmdstan_cache}")
+        if os.path.isdir(cmdstan_dir):
+            rmtree(cmdstan_dir)
+        copytree(cmdstan_cache, cmdstan_dir)
+        build_cmdstan(cmdstan_dir)
+    else:
+        cmdstanpy.install_cmdstan(
+            version=CMDSTAN_VERSION, dir=target_dir, overwrite=True
+        )
     cmdstanpy.set_cmdstan_path(cmdstan_dir)
-    model_name = 'prophet.stan'
-    target_name = 'prophet_model.bin'
+
+    model_name = "prophet.stan"
+    target_name = "prophet_model.bin"
     sm = cmdstanpy.CmdStanModel(stan_file=os.path.join(MODEL_DIR, model_name))
     sm.compile()
     copy(sm.exe_file, os.path.join(target_dir, target_name))
-    prune_cmdstan_files(cmdstan_dir)
+    # Clean up
+    for f in Path(MODEL_DIR).iterdir():
+        if f.is_file() and f.name != model_name:
+            os.remove(f)
+    prune_cmdstan(cmdstan_dir)
+
 
 def build_pystan_model(target_dir):
     import pystan
-    model_name = 'prophet.stan'
-    target_name = 'prophet_model.pkl'
+
+    model_name = "prophet.stan"
+    target_name = "prophet_model.pkl"
     with open(os.path.join(MODEL_DIR, model_name)) as f:
         model_code = f.read()
     sm = pystan.StanModel(model_code=model_code)
-    with open(os.path.join(target_dir, target_name), 'wb') as f:
+    with open(os.path.join(target_dir, target_name), "wb") as f:
         pickle.dump(sm, f, protocol=pickle.HIGHEST_PROTOCOL)
+
 
 def build_models(target_dir):
     for backend in get_backends_from_env():
@@ -77,6 +182,7 @@ def build_models(target_dir):
 
 class BinaryDistribution(Distribution):
     """Distribution which always forces a binary package with platform name"""
+
     def has_ext_modules(self):
         return True
 
@@ -96,6 +202,13 @@ class BuildPyCommand(build_py):
         build_py.run(self)
 
 
+class BuildExtCommand(build_ext):
+    """Ensure built extensions are added to the correct path in the wheel."""
+
+    def run(self):
+        pass
+
+
 class DevelopCommand(develop):
     """Custom develop command to pre-compile Stan models in-place."""
 
@@ -110,11 +223,14 @@ class DevelopCommand(develop):
 
 class TestCommand(test_command):
     user_options = [
-        ('test-module=', 'm', "Run 'test_suite' in specified module"),
-        ('test-suite=', 's',
-         "Run single test, case or suite (e.g. 'module.test_suite')"),
-        ('test-runner=', 'r', "Test runner to use"),
-        ('test-slow', 'w', "Test slow suites (default off)"),
+        ("test-module=", "m", "Run 'test_suite' in specified module"),
+        (
+            "test-suite=",
+            "s",
+            "Run single test, case or suite (e.g. 'module.test_suite')",
+        ),
+        ("test-runner=", "r", "Test runner to use"),
+        ("test-slow", "w", "Test slow suites (default off)"),
     ]
     test_slow = None
 
@@ -125,23 +241,23 @@ class TestCommand(test_command):
     def finalize_options(self):
         super(TestCommand, self).finalize_options()
         if self.test_slow is None:
-            self.test_slow = getattr(self.distribution, 'test_slow', False)
+            self.test_slow = getattr(self.distribution, "test_slow", False)
 
     """We must run tests on the build directory, not source."""
 
     def with_project_on_sys_path(self, func):
         # Ensure metadata is up-to-date
-        self.reinitialize_command('build_py', inplace=0)
-        self.run_command('build_py')
+        self.reinitialize_command("build_py", inplace=0)
+        self.run_command("build_py")
         bpy_cmd = self.get_finalized_command("build_py")
         build_path = normalize_path(bpy_cmd.build_lib)
 
         # Build extensions
-        self.reinitialize_command('egg_info', egg_base=build_path)
-        self.run_command('egg_info')
+        self.reinitialize_command("egg_info", egg_base=build_path)
+        self.run_command("egg_info")
 
-        self.reinitialize_command('build_ext', inplace=0)
-        self.run_command('build_ext')
+        self.reinitialize_command("build_ext", inplace=0)
+        self.run_command("build_ext")
 
         ei_cmd = self.get_finalized_command("egg_info")
 
@@ -152,7 +268,7 @@ class TestCommand(test_command):
             sys.path.insert(0, normalize_path(ei_cmd.egg_base))
             working_set.__init__()
             add_activation_listener(lambda dist: dist.activate())
-            require('%s==%s' % (ei_cmd.egg_name, ei_cmd.egg_version))
+            require("%s==%s" % (ei_cmd.egg_name, ei_cmd.egg_version))
             func()
         finally:
             sys.path[:] = old_path
@@ -160,37 +276,40 @@ class TestCommand(test_command):
             sys.modules.update(old_modules)
             working_set.__init__()
 
-with open('README.md', 'r', encoding='utf-8') as f:
+
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-with open('requirements.txt', 'r') as f:
+with open("requirements.txt", "r") as f:
     install_requires = f.read().splitlines()
 
 setup(
-    name='prophet',
-    version='1.0.1',
-    description='Automatic Forecasting Procedure',
-    url='https://facebook.github.io/prophet/',
-    author='Sean J. Taylor <sjtz@pm.me>, Ben Letham <bletham@fb.com>',
-    author_email='sjtz@pm.me',
-    license='MIT',
+    name="prophet",
+    version="1.0.1",
+    description="Automatic Forecasting Procedure",
+    url="https://facebook.github.io/prophet/",
+    author="Sean J. Taylor <sjtz@pm.me>, Ben Letham <bletham@fb.com>",
+    author_email="sjtz@pm.me",
+    license="MIT",
     packages=find_packages(),
     install_requires=install_requires,
-    python_requires='>=3',
+    python_requires=">=3",
     zip_safe=False,
     include_package_data=True,
+    ext_modules=[Extension("prophet.stan_model", [])],
     cmdclass={
-        'build_py': BuildPyCommand,
-        'develop': DevelopCommand,
-        'test': TestCommand,
+        "build_ext": BuildExtCommand,
+        "build_py": BuildPyCommand,
+        "develop": DevelopCommand,
+        "test": TestCommand,
     },
-    test_suite='prophet.tests',
+    test_suite="prophet.tests",
     classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
     ],
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     distclass=BinaryDistribution,
 )
