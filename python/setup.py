04e1c0ba0a606753b3bfb0646c23756cd506e78d
Anurag Kumar <mailanu98@gmail.com>
2021-09-15 14:59:06 +0530

    Update setup.py
    updated classifiers

--- python/setup.py
+++ python/setup.py
@@ -145,6 +145,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     long_description=long_description,
     long_description_content_type='text/markdown',
