57dcf06f93f2ca33ba70d2fb91ab2185db54526d
Cuong Duong <cuong.duong242@gmail.com>
2021-10-03 18:08:49 +1100

    Remove python 3.9

--- python/setup.py
+++ python/setup.py
@@ -318,7 +318,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
