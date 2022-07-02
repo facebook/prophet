e712a050217d9da0f0dcbee1f39c75e0a8b7ed1f
Cuong Duong <cuong@canva.com>
2022-07-02 18:28:54 +1000

    update serialization tests

--- python/prophet/tests/test_serialize.py
+++ python/prophet/tests/test_serialize.py
@@ -142,6 +142,7 @@ class TestSerialize(TestCase):
         old_versions = {
             '0.6.1.dev0': (29.3669923968994, 'fb'),
             '0.7.1': (29.282810844704414, 'fb'),
+            '1.0.1': (29.282810844704414, ''),
         }
         for v, (pred_val, v_str) in old_versions.items():
             fname = os.path.join(
