01d82e4e22eab2a91f9cd4edef4256f6714f7d6f
Brian Blood <bloodbrian94@gmail.com>
2022-09-14 21:17:41 +0900

    Update plot.py

--- python/prophet/plot.py
+++ python/prophet/plot.py
@@ -521,7 +521,7 @@ def plot_cross_validation_metric(
 
     # Some work because matplotlib does not handle timedelta
     # Target ~10 ticks.
-    tick_w = max(df_none['horizon'].view('timedelta64[ns]')) / 10.
+    tick_w = max(df_none['horizon'].astype('timedelta64[ns]')) / 10.
     # Find the largest time resolution that has <1 unit per bin.
     dts = ['D', 'h', 'm', 's', 'ms', 'us', 'ns']
     dt_names = [
@@ -541,8 +541,8 @@ def plot_cross_validation_metric(
         if np.timedelta64(1, dt) < np.timedelta64(tick_w, 'ns'):
             break
 
-    x_plt = df_none['horizon'].view('timedelta64[ns]').view(np.int64) / float(dt_conversions[i])
-    x_plt_h = df_h['horizon'].view('timedelta64[ns]').view(np.int64) / float(dt_conversions[i])
+    x_plt = df_none['horizon'].astype('timedelta64[ns]').view(np.int64) / float(dt_conversions[i])
+    x_plt_h = df_h['horizon'].astype('timedelta64[ns]').view(np.int64) / float(dt_conversions[i])
 
     ax.plot(x_plt, df_none[metric], '.', alpha=0.1, c=point_color)
     ax.plot(x_plt_h, df_h[metric], '-', c=color)
