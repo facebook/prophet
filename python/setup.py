bdffba16166a90c5620772f924f90c638e202a73
Cuong Duong <cuong@canva.com>
2023-01-12 10:21:00 +1100

    fix 3.10 setting

--- python/setup.py
+++ python/setup.py
@@ -263,6 +263,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     long_description=long_description,
     long_description_content_type="text/markdown",
