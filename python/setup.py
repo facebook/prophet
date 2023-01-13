ed3e56024599e79c096165cb9f0b258e7625ba53
Cuong Duong <cuong@canva.com>
2023-01-14 02:23:56 +1100

    use pyproject.toml and editable wheel install

--- python/setup.py
+++ python/setup.py
@@ -4,18 +4,15 @@
 # LICENSE file in the root directory of this source tree.
 
 import os
-import sys
 import platform
 from pathlib import Path
 from shutil import copy, copytree, rmtree
 from typing import List
 
-from pkg_resources import add_activation_listener, normalize_path, require, working_set
 from setuptools import find_packages, setup, Extension
 from setuptools.command.build_ext import build_ext
 from setuptools.command.build_py import build_py
-from setuptools.command.develop import develop
-from setuptools.command.test import test as test_command
+from setuptools.command.editable_wheel import editable_wheel
 
 
 MODEL_DIR = "stan"
@@ -52,6 +49,7 @@ def prune_cmdstan(cmdstan_dir: str) -> None:
     rmtree(original_dir)
     temp_dir.rename(original_dir)
 
+
 def repackage_cmdstan():
     return os.environ.get("PROPHET_REPACKAGE_CMDSTAN", "").lower() not in ["false", "0"]
 
@@ -59,6 +57,7 @@ def repackage_cmdstan():
 def maybe_install_cmdstan_toolchain():
     """Install C++ compilers required to build stan models on Windows machines."""
     import cmdstanpy
+
     try:
         cmdstanpy.utils.cxx_toolchain_path()
     except Exception:
@@ -122,7 +121,6 @@ def build_cmdstan_model(target_dir):
         prune_cmdstan(cmdstan_dir)
 
 
-
 def get_backends_from_env() -> List[str]:
     return os.environ.get("STAN_BACKEND", "CMDSTANPY").split(",")
 
@@ -131,8 +129,9 @@ def build_models(target_dir):
     print("Compiling cmdstanpy model")
     build_cmdstan_model(target_dir)
 
-    if 'PYSTAN' in get_backends_from_env():
+    if "PYSTAN" in get_backends_from_env():
         raise ValueError("PyStan backend is not supported for Prophet >= 1.1")
+
 
 class BuildPyCommand(build_py):
     """Custom build command to pre-compile Stan models."""
@@ -153,118 +152,33 @@ class BuildExtCommand(build_ext):
         pass
 
 
-class DevelopCommand(develop):
+class EditableWheel(editable_wheel):
     """Custom develop command to pre-compile Stan models in-place."""
 
     def run(self):
         if not self.dry_run:
-            target_dir = os.path.join(self.setup_path, MODEL_TARGET_DIR)
+            target_dir = os.path.join(self.project_dir, MODEL_TARGET_DIR)
             self.mkpath(target_dir)
             build_models(target_dir)
 
-        develop.run(self)
+        editable_wheel.run(self)
 
-
-class TestCommand(test_command):
-    user_options = [
-        ("test-module=", "m", "Run 'test_suite' in specified module"),
-        (
-            "test-suite=",
-            "s",
-            "Run single test, case or suite (e.g. 'module.test_suite')",
-        ),
-        ("test-runner=", "r", "Test runner to use"),
-        ("test-slow", "w", "Test slow suites (default off)"),
-    ]
-    test_slow = None
-
-    def initialize_options(self):
-        super(TestCommand, self).initialize_options()
-        self.test_slow = False
-
-    def finalize_options(self):
-        super(TestCommand, self).finalize_options()
-        if self.test_slow is None:
-            self.test_slow = getattr(self.distribution, "test_slow", False)
-
-    """We must run tests on the build directory, not source."""
-
-    def with_project_on_sys_path(self, func):
-        # Ensure metadata is up-to-date
-        self.reinitialize_command("build_py", inplace=0)
-        self.run_command("build_py")
-        bpy_cmd = self.get_finalized_command("build_py")
-        build_path = normalize_path(bpy_cmd.build_lib)
-
-        # Build extensions
-        self.reinitialize_command("egg_info", egg_base=build_path)
-        self.run_command("egg_info")
-
-        self.reinitialize_command("build_ext", inplace=0)
-        self.run_command("build_ext")
-
-        ei_cmd = self.get_finalized_command("egg_info")
-
-        old_path = sys.path[:]
-        old_modules = sys.modules.copy()
-
-        try:
-            sys.path.insert(0, normalize_path(ei_cmd.egg_base))
-            working_set.__init__()
-            add_activation_listener(lambda dist: dist.activate())
-            require("%s==%s" % (ei_cmd.egg_name, ei_cmd.egg_version))
-            func()
-        finally:
-            sys.path[:] = old_path
-            sys.modules.clear()
-            sys.modules.update(old_modules)
-            working_set.__init__()
-
-
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
-
-with open("requirements.txt", "r") as f:
-    install_requires = f.read().splitlines()
 
 about = {}
 here = Path(__file__).parent.resolve()
-with open(here / "prophet" /  "__version__.py", "r") as f:
+with open(here / "prophet" / "__version__.py", "r") as f:
     exec(f.read(), about)
 
 setup(
-    name=about["__title__"],
     version=about["__version__"],
-    description=about["__description__"],
-    url=about["__url__"],
-    project_urls={
-        "Source": "https://github.com/facebook/prophet",
-    },
-    author=about["__author__"],
-    author_email=about["__author_email__"],
-    license=about["__license__"],
     packages=find_packages(),
-    install_requires=install_requires,
-    python_requires=">=3.7",
     zip_safe=False,
     include_package_data=True,
     ext_modules=[Extension("prophet.stan_model", [])],
     cmdclass={
         "build_ext": BuildExtCommand,
         "build_py": BuildPyCommand,
-        "develop": DevelopCommand,
-        "test": TestCommand,
+        "editable_wheel": EditableWheel,
     },
     test_suite="prophet.tests",
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-    ],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
 )
