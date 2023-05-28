32498b6d31a44fa8c160be2fdc5640ed32cbdba6
Cuong Duong <cuong@canva.com>
2023-05-28 17:10:40 +1000

    formatting and refactor generate holidays csv

--- python/prophet/make_holidays.py
+++ python/prophet/make_holidays.py
@@ -24,12 +24,12 @@ def get_country_holidays_class(country):
     A valid country holidays class
     """
     substitutions = {
-        'TU': 'TR',  # For compatibility with Turkey as 'TU' cases.
+        "TU": "TR",  # For compatibility with Turkey as 'TU' cases.
     }
 
     country = substitutions.get(country, country)
     if not hasattr(holidays, country):
-        raise AttributeError(f'Holidays in {country} are not currently supported!')
+        raise AttributeError(f"Holidays in {country} are not currently supported!")
 
     return getattr(holidays, country)
 
@@ -46,7 +46,7 @@ def get_holiday_names(country):
     A set of all possible holiday names of given country
     """
     country_holidays = get_country_holidays_class(country)
-    return set(country_holidays(language='en_US', years=np.arange(1995, 2045)).values())
+    return set(country_holidays(language="en_US", years=np.arange(1995, 2045)).values())
 
 
 def make_holidays_df(year_list, country, province=None, state=None):
@@ -63,13 +63,14 @@ def make_holidays_df(year_list, country, province=None, state=None):
     to 'holidays' params in Prophet
     """
     country_holidays = get_country_holidays_class(country)
-    holidays = country_holidays(expand=False, language='en_US', subdiv=province, years=year_list)
+    holidays = country_holidays(expand=False, language="en_US", subdiv=province, years=year_list)
 
     holidays_df = pd.DataFrame(
-        [(date, holidays.get_list(date)) for date in holidays], columns=['ds', 'holiday'],
+        [(date, holidays.get_list(date)) for date in holidays],
+        columns=["ds", "holiday"],
     )
-    holidays_df = holidays_df.explode('holiday')
+    holidays_df = holidays_df.explode("holiday")
     holidays_df.reset_index(inplace=True, drop=True)
-    holidays_df['ds'] = pd.to_datetime(holidays_df['ds'])
+    holidays_df["ds"] = pd.to_datetime(holidays_df["ds"])
 
     return holidays_df
--- python/scripts/generate_holidays_file.py
+++ python/scripts/generate_holidays_file.py
@@ -18,34 +18,27 @@ from holidays import list_supported_countries
 from prophet.make_holidays import make_holidays_df
 
 
-def utf8_to_ascii(text):
-    """Holidays often have utf-8 characters. These are not allowed in R
-    package data (they generate a NOTE).
+def utf8_to_ascii(text: str) -> str:
+    """Holidays often have utf-8 characters. These are not allowed in R package data (they generate a NOTE).
     TODO: revisit whether we want to do this lossy conversion.
     """
-    ascii_text = (
-        unicodedata.normalize('NFD', text)
-        .encode('ascii', 'ignore')
-        .decode('ascii')
-    )
+    ascii_text = unicodedata.normalize("NFD", text).encode("ascii", "ignore").decode("ascii")
     # Remove trailing empty brackets and spaces.
     ascii_text = re.sub(r"\(\)$", "", ascii_text).strip()
 
     # Check if anything converted
-    if sum(1 for x in ascii_text if x not in [' ', '(', ')', ',']) == 0:
-        return 'FAILED_TO_PARSE'
+    if sum(1 for x in ascii_text if x not in [" ", "(", ")", ","]) == 0:
+        return "FAILED_TO_PARSE"
     else:
         return ascii_text
 
 
-def generate_holidays_file():
-    """Generate csv file of all possible holiday names, ds,
-    and countries, year combination
-    """
+def generate_holidays_df() -> pd.DataFrame:
+    """Generate csv file of all possible holiday names, ds, and countries, year combination."""
     country_codes = set(list_supported_countries().keys())
 
     # For compatibility with Turkey as 'TU' cases.
-    country_codes.add('TU')
+    country_codes.add("TU")
 
     all_holidays = []
     for country_code in country_codes:
@@ -53,24 +46,35 @@ def generate_holidays_file():
             year_list=np.arange(1995, 2045, 1).tolist(),
             country=country_code,
         )
-        df['country'] = country_code
+        df["country"] = country_code
         all_holidays.append(df)
 
     generated_holidays = pd.concat(all_holidays, axis=0, ignore_index=True)
-    generated_holidays['year'] = generated_holidays.ds.dt.year
-    generated_holidays.sort_values(['country', 'ds', 'holiday'], inplace=True)
+    generated_holidays["year"] = generated_holidays.ds.dt.year
+    generated_holidays.sort_values(["country", "ds", "holiday"], inplace=True)
 
     # Convert to ASCII, and drop holidays that fail to convert
-    generated_holidays['holiday'] = generated_holidays['holiday'].apply(utf8_to_ascii)
-    failed_countries = generated_holidays.loc[generated_holidays['holiday'] == 'FAILED_TO_PARSE', 'country'].unique()
+    generated_holidays["holiday"] = generated_holidays["holiday"].apply(utf8_to_ascii)
+    failed_countries = generated_holidays.loc[
+        generated_holidays["holiday"] == "FAILED_TO_PARSE", "country"
+    ].unique()
     if len(failed_countries) > 0:
-        print('Failed to convert UTF-8 holidays for:')
-        print('\n'.join(failed_countries))
-    assert 'FAILED_TO_PARSE' not in generated_holidays['holiday'].unique()
+        print("Failed to convert UTF-8 holidays for:")
+        print("\n".join(failed_countries))
+    assert "FAILED_TO_PARSE" not in generated_holidays["holiday"].unique()
+    return generated_holidays
 
-    generated_holidays.to_csv('../R/data-raw/generated_holidays.csv', index=False)
 
+if __name__ == "__main__":
+    import argparse
+    import pathlib
 
-if __name__ == '__main__':
+    if not pathlib.Path.cwd().stem == "python":
+        raise RuntimeError("Run script from prophet/python directory")
+    OUT_CSV_PATH = pathlib.Path(".") / ".." / "R/data-raw/generated_holidays.csv"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--outfile", default=OUT_CSV_PATH)
+    args = parser.parse_args()
     # execute only if run as a script
-    generate_holidays_file()
+    df = generate_holidays_df()
+    df.to_csv(args.outfile, index=False)
