93cfd2dfd0c7f0fd9ccd4eeef0a52986778c79ae
Cuong Duong <cuong.duong242@gmail.com>
2023-07-11 21:36:29 +1000

    refactor 1

--- python/prophet/models.py
+++ python/prophet/models.py
@@ -6,17 +6,20 @@
 
 from __future__ import absolute_import, division, print_function
 from abc import abstractmethod, ABC
-from typing import Tuple
+from ast import Num
+
+from typing import Tuple, Dict, Union
 from collections import OrderedDict
 from enum import Enum
-from .numpyro_model import find_map, run_mcmc
 import importlib_resources
 import platform
 
 import logging
-logger = logging.getLogger('prophet.models')
+
+logger = logging.getLogger("prophet.models")
 
 PLATFORM = "win" if platform.platform().startswith("Win") else "unix"
+
 
 class IStanBackend(ABC):
     def __init__(self):
@@ -30,11 +33,10 @@ class IStanBackend(ABC):
          * newton_fallback [bool]: whether to fallback to Newton if L-BFGS fails
         """
         for k, v in kwargs.items():
-            if k == 'newton_fallback':
+            if k == "newton_fallback":
                 self.newton_fallback = v
             else:
-                raise ValueError(f'Unknown option {k}')
-
+                raise ValueError(f"Unknown option {k}")
 
     @staticmethod
     @abstractmethod
@@ -56,10 +58,14 @@ class IStanBackend(ABC):
 
 class CmdStanPyBackend(IStanBackend):
     CMDSTAN_VERSION = "2.31.0"
+
     def __init__(self):
         import cmdstanpy
+
         # this must be set before super.__init__() for load_model to work on Windows
-        local_cmdstan = importlib_resources.files("prophet") / "stan_model" / f"cmdstan-{self.CMDSTAN_VERSION}"
+        local_cmdstan = (
+            importlib_resources.files("prophet") / "stan_model" / f"cmdstan-{self.CMDSTAN_VERSION}"
+        )
         if local_cmdstan.exists():
             cmdstanpy.set_cmdstan_path(str(local_cmdstan))
         super().__init__()
@@ -70,19 +76,20 @@ class CmdStanPyBackend(IStanBackend):
 
     def load_model(self):
         import cmdstanpy
+
         model_file = importlib_resources.files("prophet") / "stan_model" / "prophet_model.bin"
         return cmdstanpy.CmdStanModel(exe_file=str(model_file))
 
     def fit(self, stan_init, stan_data, **kwargs):
-        if 'inits' not in kwargs and 'init' in kwargs:
-            stan_init = self.sanitize_custom_inits(stan_init, kwargs['init'])
-            del kwargs['init']
+        if "inits" not in kwargs and "init" in kwargs:
+            stan_init = self.sanitize_custom_inits(stan_init, kwargs["init"])
+            del kwargs["init"]
 
         inits_list, data_list = self.prepare_data(stan_init, stan_data)
         args = dict(
             data=data_list,
             inits=inits_list,
-            algorithm='Newton' if data_list['T'] < 100 else 'LBFGS',
+            algorithm="Newton" if data_list["T"] < 100 else "LBFGS",
             iter=int(1e4),
         )
         args.update(kwargs)
@@ -91,33 +98,34 @@ class CmdStanPyBackend(IStanBackend):
             self.stan_fit = self.model.optimize(**args)
         except RuntimeError as e:
             # Fall back on Newton
-            if not self.newton_fallback or args['algorithm'] == 'Newton':
+            if not self.newton_fallback or args["algorithm"] == "Newton":
                 raise e
-            logger.warning('Optimization terminated abnormally. Falling back to Newton.')
-            args['algorithm'] = 'Newton'
+            logger.warning("Optimization terminated abnormally. Falling back to Newton.")
+            args["algorithm"] = "Newton"
             self.stan_fit = self.model.optimize(**args)
         params = self.stan_to_dict_numpy(
-            self.stan_fit.column_names, self.stan_fit.optimized_params_np)
+            self.stan_fit.column_names, self.stan_fit.optimized_params_np
+        )
         for par in params:
             params[par] = params[par].reshape((1, -1))
         return params
 
     def sampling(self, stan_init, stan_data, samples, **kwargs) -> dict:
-        if 'inits' not in kwargs and 'init' in kwargs:
-            stan_init = self.sanitize_custom_inits(stan_init, kwargs['init'])
-            del kwargs['init']
+        if "inits" not in kwargs and "init" in kwargs:
+            stan_init = self.sanitize_custom_inits(stan_init, kwargs["init"])
+            del kwargs["init"]
 
         inits_list, data_list = self.prepare_data(stan_init, stan_data)
         args = dict(
             data=data_list,
             inits=inits_list,
         )
-        if 'chains' not in kwargs:
-            kwargs['chains'] = 4
+        if "chains" not in kwargs:
+            kwargs["chains"] = 4
         iter_half = samples // 2
-        kwargs['iter_sampling'] = iter_half
-        if 'iter_warmup' not in kwargs:
-            kwargs['iter_warmup'] = iter_half
+        kwargs["iter_sampling"] = iter_half
+        if "iter_warmup" not in kwargs:
+            kwargs["iter_warmup"] = iter_half
         args.update(kwargs)
 
         self.stan_fit = self.model.sample(**args)
@@ -131,7 +139,7 @@ class CmdStanPyBackend(IStanBackend):
             if s[1] == 1:
                 params[par] = params[par].reshape((s[0],))
 
-            if par in ['delta', 'beta'] and len(s) < 2:
+            if par in ["delta", "beta"] and len(s) < 2:
                 params[par] = params[par].reshape((-1, 1))
 
         return params
@@ -140,12 +148,12 @@ class CmdStanPyBackend(IStanBackend):
     def sanitize_custom_inits(default_inits, custom_inits):
         """Validate that custom inits have the correct type and shape, otherwise use defaults."""
         sanitized = {}
-        for param in ['k', 'm', 'sigma_obs']:
+        for param in ["k", "m", "sigma_obs"]:
             try:
                 sanitized[param] = float(custom_inits.get(param))
             except Exception:
                 sanitized[param] = default_inits[param]
-        for param in ['delta', 'beta']:
+        for param in ["delta", "beta"]:
             if default_inits[param].shape == custom_inits[param].shape:
                 sanitized[param] = custom_inits[param]
             else:
@@ -156,32 +164,32 @@ class CmdStanPyBackend(IStanBackend):
     def prepare_data(init, data) -> Tuple[dict, dict]:
         """Converts np.ndarrays to lists that can be read by cmdstanpy."""
         cmdstanpy_data = {
-            'T': data['T'],
-            'S': data['S'],
-            'K': data['K'],
-            'tau': data['tau'],
-            'trend_indicator': data['trend_indicator'],
-            'y': data['y'].tolist(),
-            't': data['t'].tolist(),
-            'cap': data['cap'].tolist(),
-            't_change': data['t_change'].tolist(),
-            's_a': data['s_a'].tolist(),
-            's_m': data['s_m'].tolist(),
-            'X': data['X'].to_numpy().tolist(),
-            'sigmas': data['sigmas']
+            "T": data["T"],
+            "S": data["S"],
+            "K": data["K"],
+            "tau": data["tau"],
+            "trend_indicator": data["trend_indicator"],
+            "y": data["y"].tolist(),
+            "t": data["t"].tolist(),
+            "cap": data["cap"].tolist(),
+            "t_change": data["t_change"].tolist(),
+            "s_a": data["s_a"].tolist(),
+            "s_m": data["s_m"].tolist(),
+            "X": data["X"].to_numpy().tolist(),
+            "sigmas": data["sigmas"],
         }
 
         cmdstanpy_init = {
-            'k': init['k'],
-            'm': init['m'],
-            'delta': init['delta'].tolist(),
-            'beta': init['beta'].tolist(),
-            'sigma_obs': init['sigma_obs']
+            "k": init["k"],
+            "m": init["m"],
+            "delta": init["delta"].tolist(),
+            "beta": init["beta"].tolist(),
+            "sigma_obs": init["sigma_obs"],
         }
         return (cmdstanpy_init, cmdstanpy_data)
 
     @staticmethod
-    def stan_to_dict_numpy(column_names: Tuple[str, ...], data: 'np.array'):
+    def stan_to_dict_numpy(column_names: Tuple[str, ...], data: "np.array"):
         import numpy as np
 
         output = OrderedDict()
@@ -199,9 +207,7 @@ class CmdStanPyBackend(IStanBackend):
 
             if curr != prev:
                 if prev in output:
-                    raise RuntimeError(
-                        "Found repeated column name"
-                    )
+                    raise RuntimeError("Found repeated column name")
                 if two_dims:
                     output[prev] = np.array(data[:, start:end])
                 else:
@@ -210,9 +216,7 @@ class CmdStanPyBackend(IStanBackend):
                 start = end
             end += 1
         if prev in output:
-            raise RuntimeError(
-                "Found repeated column name"
-            )
+            raise RuntimeError("Found repeated column name")
         if two_dims:
             output[prev] = np.array(data[:, start:end])
         else:
@@ -220,9 +224,14 @@ class CmdStanPyBackend(IStanBackend):
         return output
 
 
-
-
 class NumpyroBackend(IStanBackend):
+    def __init__(self):
+        try:
+            import numpyro
+        except ImportError as exc:
+            raise Exception("numpyro not found, please try pip install prophet[numpyro]") from exc
+        super().__init__()
+        self.newton_fallback = False
 
     @staticmethod
     def get_type():
@@ -231,14 +240,87 @@ class NumpyroBackend(IStanBackend):
     def load_model(self):
         """No-op since this backend does not rely on a stan model."""
 
+    @staticmethod
+    def prepare_data(
+        init: Dict[str, Union[float, int, list]], data: Dict[str, Union[float, int, list]]
+    ) -> Tuple[dict, dict]:
+        import jax.numpy as jnp
+        from .numpyro_model import NumpyroModelInput, NumpyroModelParams
+
+        data = {k: v for k, v in data.items() if k in NumpyroModelInput.__annotations__}
+        for var in data:
+            if NumpyroModelInput.__annotations__[var] == jnp.ndarray:
+                data[var] = jnp.array(data[var])
+
+        init = {k: v for k, v in init.items() if k in NumpyroModelParams.__annotations__}
+        for var in init:
+            if NumpyroModelParams.__annotations__[var] == jnp.ndarray:
+                init[var] = jnp.array(init[var])
+        return init, data
+
     def fit(self, stan_init, stan_data, **kwargs) -> dict:
-        return find_map(stan_data, stan_init)
+        import jax
+        import numpy as np
+        from numpyro.infer import SVI
+        from numpyro.infer.autoguide import AutoDelta, Trace_ELBO
+        from numpyro.optim import Adam
+        from .numpyro_model import model
 
-    def sampling(self, stan_init, stan_data, samples, **kwargs) -> dict:
-        return run_mcmc(stan_data, samples)
+        jax.config.update("jax_enable_x64", True)
 
-    def build_model(target_dir, model_dir):
-        """No-op since this backend does not rely on a stan model."""
+        init, data = self.prepare_data(stan_init, stan_data)
+        guide = AutoDelta(model)
+        optimizer = Adam(0.001)
+        svi = SVI(model, guide, optimizer, loss=Trace_ELBO())
+        rng_key = jax.random.PRNGKey(0)
+        run_params = {"num_steps": 10000, "progress_bar": False}
+        run_params.update(kwargs)
+
+        svi_results = svi.run(
+            rng_key=rng_key,
+            num_steps=run_params["num_steps"],
+            progress_bar=run_params["progress_bar"],
+            init_params=init,
+            **data,
+        )
+        self.stan_fit = svi_results
+        return {
+            k.replace("_auto_loc", ""): np.array(v).reshape((1, -1))
+            for k, v in svi_results.params.items()
+        }
+
+    def sampling(self, stan_init, stan_data, samples: int, **kwargs) -> dict:
+        import jax
+        import jax.numpy as jnp
+        import numpy as np
+        from numpyro.infer import NUTS, MCMC
+        from numpyro import set_host_device_count
+        from .numpyro_model import model
+
+        NUM_CHAINS = 4
+        jax.config.update("jax_enable_x64", True)
+        set_host_device_count(NUM_CHAINS)
+
+        init, data = self.prepare_data(stan_init, stan_data)
+        init = {k: jnp.vstack([jnp.array(v) for _ in range(NUM_CHAINS)]) for k, v in init.items()}
+        for var in ["T", "K", "S", "trend_indicator"]:
+            data[var] = float(data[var])
+        nuts_kernel = NUTS(model)
+        run_params = {"chain_method": "parallel", "progress_bar": False}
+        run_params.update(kwargs)
+        mcmc = MCMC(
+            nuts_kernel,
+            num_warmup=samples // 2,
+            num_samples=samples // 2,
+            thinning=1,
+            num_chains=4,
+            chain_method=run_params["chain_method"],
+            progress_bar=run_params["progress_bar"],
+        )
+        rng_key = jax.random.PRNGKey(0)
+        mcmc.run(rng_key=rng_key, init_params=init, **data)
+        self.stan_fit = mcmc
+        return {k: np.array(v) for k, v in mcmc.get_samples(group_by_chain=False).items()}
 
 
 class StanBackendEnum(Enum):
--- python/prophet/numpyro_model.py
+++ python/prophet/numpyro_model.py
@@ -1,19 +1,154 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
 from jax import jit
 from jax.lax import scan
 import jax.numpy as jnp
 from jax.scipy.special import expit
 import numpy as np
 import numpyro.distributions as dist
-from numpyro.infer import MCMC, NUTS
 from numpyro import sample, plate, deterministic
-from numpyro.infer import SVI
-from numpyro.infer.autoguide import AutoDelta, Trace_ELBO
-from numpyro.infer.initialization import init_to_value
-from numpyro.optim import Minimize
-from jax import random
+
+from typing import Callable, Literal, Optional, Tuple
 
 
-def construct_changepoint_matrix(t, t_change, T, S):
+class Trend(ABC):
+    def __init__(
+        self,
+        k: float,
+        m: float,
+        delta: jnp.ndarray,
+        t: jnp.ndarray,
+        cap: Optional[jnp.ndarray],
+        A: jnp.ndarray,
+        t_change: jnp.ndarray,
+        S: int,
+    ):
+        """
+        Parameters
+        ----------
+        k: The base growth rate.
+        m: The offset parameter for the trend curve.
+        delta: An array of length equal to the number of changepoints. Each element is the change in the growth rate at t_change.
+        t: An array of normalized time values.
+        cap: The capacity at each time point t.
+        A: The changepoint matrix.
+        t_change: The normalized time values at which changepoints are assumed to occur.
+        S: The number of changepoints.
+        """
+        self.k = k
+        self.m = m
+        self.delta = delta
+        self.t = t
+        self.cap = cap
+        self.A = A
+        self.t_change = t_change
+        self.S = S
+
+    @abstractmethod
+    def compute_values(self) -> jnp.ndarray:
+        pass
+
+
+@jit
+def linear_trend(k, m, delta, t, t_change, A) -> jnp.ndarray:
+    """
+    Calculates the trend value at each time point t assuming linear growth. The trend function g(t) is defined
+    in equation 4 on page 10 of the original Forecasting at Scale paper: https://peerj.com/preprints/3190.pdf
+
+    Returns
+    -------
+    An array of trend values at each time point.
+    """
+    time_dependent = jnp.multiply((k + A.dot(delta)), t)
+    constant = m + A.dot(jnp.multiply(-t_change, delta))
+    return time_dependent + constant
+
+
+def transition_function(
+    carry: Tuple[float, np.ndarray, np.ndarray], index: int
+) -> Tuple[Tuple[float, np.ndarray, np.ndarray], float]:
+    """
+    Helper function for calculating the offset parameter adjustments required at each changepoint.
+
+    Parameters
+    ----------
+    carry: A tuple containing the previous offset parameter value, the changepoint times,
+        and the cumulative growth rate after each changepoint.
+    index: The index of the current changepoint.
+
+    Returns
+    -------
+    A tuple containing:
+        - A tuple containing the updated offset parameter value, the changepoint times, and the cumulative growth rate at each changepoint.
+        - The offset parameter adjustment required at the current changepoint.
+    """
+    m_pr, t_change, k_s = carry
+    gamma_i = (t_change[index] - m_pr) * (1 - k_s[index] / k_s[index + 1])
+    return (m_pr + gamma_i, t_change, k_s), gamma_i
+
+
+def compute_gamma(k, m, delta, t_change, S) -> jnp.ndarray:
+    """
+    Calculates the offset parameter adjustments (gamma_j) required at each changepoint assuming logistic growth.
+    The equation for gamma_j is defined in page 9 of the original Forecasting at Scale paper: https://peerj.com/preprints/3190.pdf.
+
+    Parameters
+    ----------
+    k: The base growth rate.
+    m: The offset parameter for the trend curve.
+    delta: An array of length equal to the number of changepoints. Each element is the change in the growth rate at t_change.
+    t_change: The normalized time values at which changepoints are assumed to occur.
+    S: The number of changepoints.
+
+    Returns
+    -------
+    An array of offset parameter adjustments required at each changepoint.
+    """
+    k_s = jnp.append(jnp.array(k), k + jnp.cumsum(delta))
+    _, gamma = scan(transition_function, (m, t_change, k_s), jnp.arange(S))
+    return gamma
+
+
+@jit
+def logistic_trend(k, m, delta, t, cap, t_change, S, A) -> jnp.ndarray:
+    """
+    Calculates the trend value at each time point t assuming logistic growth.
+    The trend function g(t) is defined in equation 3 on page 9 of the original Forecasting at Scale paper: https://peerj.com/preprints/3190.pdf.
+
+    Returns
+    -------
+    An array of trend values at each time point.
+    """
+    gamma = compute_gamma(k, m, delta, t_change, S)
+    inv_logit = expit(jnp.multiply(k + A.dot(delta), (t - (m + A.dot(gamma)))))
+    return jnp.multiply(cap, inv_logit)
+
+
+@jit
+def flat_trend(m, T) -> jnp.ndarray:
+    return jnp.repeat(m, T)
+
+
+def construct_changepoint_matrix(
+    t: np.ndarray, t_change: np.ndarray, T: int, S: int
+) -> jnp.ndarray:
+    """
+    Constructs the A(t) indicator matrix for changepoints as defined in page 9 of the original
+    Forecasting at Scale paper: https://peerj.com/preprints/3190.pdf
+
+    Parameters
+    ----------
+    t: An array of normalized time values.
+    t_change: The normalized time values at which changepoints are assumed to occur.
+    T: The number of time points.
+    S: The number of changepoints.
+
+    Returns
+    -------
+    A matrix of shape (number of time points, number of changepoints). For each row (i.e. each time point),
+    the value of column j is 1 if changepoint j has taken effect by that time point, and 0 otherwise.
+    """
     A = np.zeros((T, S))
     a_row = np.zeros(S)
     cp_idx = 1 if S > 1 else 0
@@ -26,41 +161,37 @@ def construct_changepoint_matrix(t, t_change, T, S):
 
 
 @jit
-def linear_trend(k: float, m: float, delta: jnp.ndarray, t: jnp.ndarray, A: jnp.ndarray, t_change: jnp.ndarray):
-    first_term = jnp.multiply((k + A.dot(delta)), t)
-    second_term = (m + A.dot(jnp.multiply(-t_change, delta)))
-    return first_term + second_term
+def compute_mu(
+    trend: jnp.ndarray, X: jnp.ndarray, betas: jnp.ndarray, s_m: jnp.ndarray, s_a: jnp.ndarray
+) -> jnp.ndarray:
+    """
+    Calculates the expected value of the response variable y at each time point t.
 
-
-def transition_function(carry, index):
-    m_pr, t_change, k_s = carry
-    gamma_i = (t_change[index] - m_pr) * (1 - k_s[index] / k_s[index + 1])
-    return (m_pr + gamma_i, t_change, k_s), gamma_i
-
-
-def logistic_gamma(k, m, delta, t_change, S):
-    k_s = jnp.append(jnp.array(k), k + jnp.cumsum(delta))
-    _, gamma = scan(transition_function, (m, t_change, k_s), jnp.arange(S))
-    return gamma
-
-
-def logistic_trend(k, m, delta, t, cap, A, t_change, S):
-    gamma = logistic_gamma(k, m, delta, t_change, S)
-    inv_logit = expit(jnp.multiply(k + A.dot(delta), (t - (m + A.dot(gamma)))))
-    return jnp.multiply(cap, inv_logit)
-
-
-@jit
-def compute_mu(trend, X, betas, s_m, s_a):
+    Parameters
+    ----------
+    trend: The trend value at each time point t.
+    X: The matrix containing fourier values representing seasonality, holidays indicators, and exogenous regressor values.
+    betas: The coefficients for the features in X.
+    s_m: An indicator matrix for the features in X that have a multiplicative effect on y.
+    s_a: An indicator matrix for the features in X that have an additive effect on y.
+    """
     return jnp.multiply(trend, (1 + X.dot(betas * s_m))) + X.dot(betas * s_a)
 
 
-def model_linear(s_m, s_a, sigmas, X, cap, t, t_change, tau, T, S, y, changepoint):
+def model(T, K, t, cap, y, S, t_change, X, sigmas, tau, trend_indicator, s_a, s_m) -> None:
+    A = construct_changepoint_matrix(np.array(t), np.array(t_change), T, S)
     k = sample("k", dist.Normal(0, 5))
     m = sample("m", dist.Normal(0, 5))
-    with plate('delta_dim', S):
+    with plate("delta_dim", S):
         delta = sample("delta", dist.Laplace(0, tau))
-        trend = deterministic("trend", linear_trend(k, m, delta, t, changepoint, t_change))
+    if trend_indicator == 0:
+        trend = deterministic("trend", linear_trend(k, m, delta, t, t_change, A))
+    elif trend_indicator == 1:
+        trend = deterministic("trend", logistic_trend(k, m, delta, t, cap, t_change, S, A))
+    elif trend_indicator == 2:
+        trend = deterministic("trend", flat_trend(m, T))
+    else:
+        raise ValueError("Invalid trend_indicator value")
     sigma_obs = sample("sigma_obs", dist.HalfNormal(scale=0.5))
     betas = sample("beta", dist.MultivariateNormal(0, jnp.diag(sigmas)))
     mu = deterministic("mu", compute_mu(trend, X, betas, s_m, s_a))
@@ -68,71 +199,45 @@ def model_linear(s_m, s_a, sigmas, X, cap, t, t_change, tau, T, S, y, changepoin
         sample("obs", dist.Normal(mu, sigma_obs), obs=y)
 
 
-def model_logistic(s_m, s_a, sigmas, X, cap, t, t_change, tau, T, S, y, changepoint):
-    k = sample("k", dist.Normal(0, 5))
-    m = sample("m", dist.Normal(0, 5))
-    with plate('delta_dim', S):
-        delta = sample("delta", dist.Laplace(0, tau))
-        trend = deterministic("trend", logistic_trend(k, m, delta, t, cap, changepoint, t_change, S))
-    sigma_obs = sample("sigma_obs", dist.HalfNormal(scale=0.5))
-    betas = sample("beta", dist.MultivariateNormal(0, jnp.diag(sigmas)))
-    mu = deterministic("mu", compute_mu(trend, X, betas, s_m, s_a))
-    with plate("data", T):
-        sample("obs", dist.Normal(mu, sigma_obs), obs=y)
+@dataclass
+class NumpyroModelInput:
+    # Number of time points
+    T: int
+    # Number of regressors (seasonality components, holidays, extra regressors)
+    K: int
+    # Time points, shape (T,)
+    t: jnp.ndarray
+    # Capacity at each time point, shape (T,). For logistic growth
+    cap: jnp.ndarray
+    # Response variable, shape (T,)
+    y: jnp.ndarray
+    # Number of changepoints
+    S: int
+    # Time points at which changepoints occur, shape (S,)
+    t_change: jnp.ndarray
+    # Matrix of regressor values at each time point, shape (T, K)
+    X: jnp.ndarray
+    # Scale on the prior distribution for each regressor coefficient, shape (K,)
+    sigmas: jnp.ndarray
+    # Scale on the prior distribution for the changepoint deltas (same for all deltas)
+    tau: float
+    # Indicator for the trend function to use (0: linear, 1: logistic, 2: flat)
+    trend_indicator: Literal[0, 1, 2]
+    # Indicator matrix for regressors with additive effect, shape (K,)
+    s_a: jnp.ndarray
+    # Indicator matrix for regressors with multiplicative effect, shape (K,)
+    s_m: jnp.ndarray
 
 
-def _dat_to_jax_arrays(dat):
-    y = jnp.array(dat['y'])
-    X = jnp.array(dat['X'])
-    t = jnp.array(dat['t'])
-    s_m = jnp.array(dat['s_m'])
-    s_a = jnp.array(dat['s_a'])
-    sigmas = jnp.array(dat['sigmas'])
-    t_change = jnp.array(dat['t_change'])
-    cap = jnp.array(dat['cap'])
-    return y, X, t, s_m, s_a, sigmas, t_change, cap
-
-
-def run_mcmc(dat, n_iterations):
-    changepoint = construct_changepoint_matrix(dat['t'], dat['t_change'], dat['T'], dat['S'])
-
-    if dat['trend_indicator'] == 0:
-        nuts_kernel = NUTS(model_linear)
-    else:
-        nuts_kernel = NUTS(model_logistic)
-
-    mcmc = MCMC(nuts_kernel, num_warmup=n_iterations, num_samples=n_iterations)
-
-    rng_key = random.PRNGKey(0)
-    y, X, t, s_m, s_a, sigmas, t_change, cap = _dat_to_jax_arrays(dat)
-
-    mcmc.run(rng_key, s_m, s_a, sigmas, X, cap, t, t_change, dat['tau'], dat['T'], dat['S'], y, changepoint)
-    samples = mcmc.get_samples()
-    return {k: np.array(samples[k]) for k in samples}
-
-
-def find_map(dat, init_args):
-    changepoint = construct_changepoint_matrix(dat['t'], dat['t_change'], dat['T'], dat['S'])
-    y, X, t, s_m, s_a, sigmas, t_change, cap = _dat_to_jax_arrays(dat)
-
-    optimizer = Minimize(method="BFGS")
-
-    init_fn = init_to_value(values={k: jnp.array(v) for k, v in init_args.items()})
-
-    if dat['trend_indicator'] == 0:
-        guide = AutoDelta(model_linear, init_loc_fn=init_fn)
-        model_fn = model_linear
-    else:
-        guide = AutoDelta(model_logistic, init_loc_fn=init_fn)
-        model_fn = model_logistic
-
-    svi = SVI(model_fn, guide, optimizer, loss=Trace_ELBO())
-
-    init_state = svi.init(random.PRNGKey(0), s_m, s_a, sigmas, X, cap, t,
-                          t_change, dat['tau'], dat['T'], dat['S'], y, changepoint)
-    optimal_state, loss = svi.update(init_state, s_m, s_a, sigmas, X, cap, t,
-                                     t_change, dat['tau'], dat['T'], dat['S'], y, changepoint)
-    params = svi.get_params(optimal_state)
-    params = {k.replace("_auto_loc", ""): np.array(v).reshape((1, -1)) for k, v in params.items()}
-    return params
-
+@dataclass
+class NumpyroModelParams:
+    # Base rate of change of the trend function
+    k: float
+    # Offset for the trend function
+    m: float
+    # The difference applied to the rate of change at each changepoint. Size (num_changepoints, )
+    delta: jnp.ndarray
+    # The standard deviation of the response (y) at each timepoint. Same for all time points.
+    sigma_obs: float
+    # The coefficients of the seasonality, holidays, and exogenous regressors. Size (num_regressors,)
+    beta: jnp.ndarray
