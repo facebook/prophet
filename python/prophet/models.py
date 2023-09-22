441d77e142b692bfa7eff02a9ae5471ed9861771
Joanne Sun <joanne.sun@walmart.com>
2023-09-22 09:38:37 -0700

    2.33.1

--- python/prophet/models.py
+++ python/prophet/models.py
@@ -54,7 +54,7 @@ class IStanBackend(ABC):
 
 
 class CmdStanPyBackend(IStanBackend):
-    CMDSTAN_VERSION = "2.33.0"
+    CMDSTAN_VERSION = "2.33.1"
     def __init__(self):
         import cmdstanpy
         # this must be set before super.__init__() for load_model to work on Windows
