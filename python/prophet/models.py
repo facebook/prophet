39b3ba2df422e2c2fd05c3ac8128215742cd8af0
Brian Ward <brianmward99@gmail.com>
2025-09-08 16:02:46 -0400

    Fix missing version location

--- python/prophet/models.py
+++ python/prophet/models.py
@@ -90,7 +90,7 @@ class IStanBackend(ABC):
 
 
 class CmdStanPyBackend(IStanBackend):
-    CMDSTAN_VERSION = "2.33.1"
+    CMDSTAN_VERSION = "2.37.0"
     def __init__(self):
         import cmdstanpy
         # this must be set before super.__init__() for load_model to work on Windows
