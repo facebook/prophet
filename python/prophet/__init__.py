3b09a61903b9462afcbd20a23570e569030b4630
jorenham <jhammudoglu@gmail.com>
2026-01-27 17:52:48 +0100

    simplified `__init__.py`

--- python/prophet/__init__.py
+++ python/prophet/__init__.py
@@ -4,11 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree. An additional grant
 # of patent rights can be found in the PATENTS file in the same directory.
+
+from prophet.__version__ import __version__
 from prophet.forecaster import Prophet
 
-from pathlib import Path
-about = {}
-here = Path(__file__).parent.resolve()
-with open(here / "__version__.py", "r") as f:
-    exec(f.read(), about)
-__version__ = about["__version__"]
+__all__ = ["Prophet", "__version__"]
